2312cf6f6e4516d0c9c732bac2479d98e9a3cce8
firdausrazamd <mfirdausraza.it@gmail.com>
2025-09-01 15:25:06 +0530

    SearchInAMatrix problems add another approach

--- src/com/codecafe/aditya_verma/binary_search/SearchInAMatrix.java
+++ src/com/codecafe/aditya_verma/binary_search/SearchInAMatrix.java
@@ -19,8 +19,7 @@ public class SearchInAMatrix {
         int start = 0, end = m*n-1;
         while(start<=end){
             int mid = start + (end - start) / 2;
-            int i = mid / n;
-            int j = mid % n;
+            int i = mid / n, j = mid % n;
             int curr = matrix[i][j];
             if(curr == target) return true;
             else if(curr > target) end = mid-1;
